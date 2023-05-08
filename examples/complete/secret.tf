be4188576cd22d05b0822cc8d92b48e00147e975
mani <mpanneerselvam@sigmasolve.net>
2023-05-08 23:58:28 +0530

    change in secret

--- examples/complete/secret.tf
+++ examples/complete/secret.tf
@@ -43,7 +43,7 @@ module "secret" {
   kms_key_enable_key_rotation     = var.kms_key_enable_key_rotation
   secret_ignore_changes           = false
   secret_read_principals          = {}
-  secret_string                   = ""
+  secret_string                   = null
   sns_pub_principals              = null
   sns_sub_principals              = null
 }
