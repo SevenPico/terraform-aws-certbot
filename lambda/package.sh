d108b656bf91a46909a6afc6b625c3c7d87ddf75
mani <mpanneerselvam@sigmasolve.net>
2023-05-11 01:40:17 +0530

    change in lambda

--- lambda/package.sh
+++ lambda/package.sh
@@ -4,9 +4,9 @@ set -e
 
 #readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly SCRIPT_DIR="/tmp/certbot"
-mkdir -p $SCRIPT_DIR
+#mkdir -p $SCRIPT_DIR
 cp /var/task/requirements.txt /tmp/certbot/
-cp /var/task/certbot-1.17.0.zip /tmp/certbot/
+
 ls -lrt /tmp/certbot/
 readonly CERTBOT_VERSION=$( awk -F= '$1 == "certbot"{ print $NF; }' "${SCRIPT_DIR}/requirements.txt" )
 readonly VENV="/certbot/venv"
