d4acd7a1a13d6cba00634266a6bf58d91231e975
mani <mpanneerselvam@sigmasolve.net>
2023-05-17 17:23:27 +0530

    modified SSL

--- examples/complete/ssl.tf
+++ examples/complete/ssl.tf
@@ -34,19 +34,18 @@ module "ssl_certificate_context" {
 # SSL Certificate
 # ------------------------------------------------------------------------------
 module "ssl_certificate" {
-  #source  = "registry.terraform.io/SevenPico/ssl-certificate/aws"
-  #version = "8.0.9"
-  source = "ssl-certificate/"
+  source  = "registry.terraform.io/SevenPico/ssl-certificate/aws"
+  version = "8.0.9"
   context = module.ssl_certificate_context.self
 
   additional_dns_names                = []
   additional_secrets                  = { EXAMPLE = "example value" }
-  create_mode                         = "Create_Secret_Only"
+  create_mode                         = "LetsEncrypt"
   create_secret_update_sns            = true
-  import_filepath_certificate         = "${path.module}/cert-values/cert.pem"
-  import_filepath_certificate_chain   = "${path.module}/cert-values/chain.pem"
-  import_filepath_csr                 = "${path.module}/cert-values/csr.pem"
-  import_filepath_private_key         = "${path.module}/cert-values/key.pem"
+  import_filepath_certificate         = null
+  import_filepath_certificate_chain   = null
+  import_filepath_csr                 = null
+  import_filepath_private_key         = null
   import_secret_arn                   = null
   keyname_certificate                 = "CERTIFICATE"
   keyname_certificate_chain           = "CERTIFICATE_CHAIN"
